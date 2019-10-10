4f7a3806ec06f25f073cae46f2b3ab0e2d99cde1
Janek Boll <janek.boll@gmx.de>
2019-10-10 11:58:56 +0000

    Auf tatsächliche Datenbank angepasst

--- firebase-app.js
+++ firebase-app.js
@@ -29,7 +29,7 @@ firebase.initializeApp({
 });
 
 const db = firebase.firestore();
-
+/*
 db.collection('/Standort').doc('CLZ2/')
 	.onSnapshot({
 	 includeMetadataChanges : false		
@@ -40,3 +40,53 @@ db.collection('/Standort').doc('CLZ2/')
 		updateDataStructure(recievedData);
 	}
 );
+*/
+
+//Funktioniert so nicht, da Firebase nur auf änderungen im eigentlichen 
+//Dokument achtet, nicht auf referenzierte
+
+db.collection("/company").doc("yDOcLJggM9S9nUNt1SuQ")
+	.collection("location").doc("yti5YJIX1Cnw0Pek79en")
+	.onSnapshot({
+		includeMetadataChanges: true
+	}, function(doc){
+			recievedData = doc.data();
+			//console.log(recievedData.Services)
+			console.log(recievedData.Services);
+			//Nicht hier, aber so ähnlich
+			//Funktioniert
+			recievedData.Services.forEach(function(service){
+				db.collection("/company").doc(service)
+				.onSnapshot({
+					includeMetadataChanges:false
+					},function(doc){
+						console.log(doc.data())
+					})
+			})
+		}
+);
+
+
+//Alternativer Ansatz:
+// Services an einem Standort sind (mehr oder weniger) statisch
+// => für alle Services von Hand listener setzen
+// nicht schön aber funktioniert 
+/*
+db.collection("/company").doc("yDOcLJggM9S9nUNt1SuQ/service/GEANbCXCkdk6T3ke2e0W")
+	.onSnapshot({
+		includeMetadataChanges:false
+		}, function(doc){
+			recievedData = doc.data();
+			console.log(recievedData);
+		}
+);
+
+db.collection("/company").doc("yDOcLJggM9S9nUNt1SuQ/service/hYdSuDsb6ptXEW4eqteH")
+	.onSnapshot({
+		includeMetadataChanges:false
+		},function(doc){
+			recievedData = doc.data();
+			console.log(recievedData);
+		}
+);
+*/
