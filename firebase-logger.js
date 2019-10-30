be5e8cd2c7d892ee62a670497bc01246eaef9937
jbol14 <janek.boll@gmx.de>
2019-10-30 17:07:24 +0100

    Logger: prüfen ob objekt id und counter hat falls ja, an Firebase senden

--- firebase-logger.js
+++ firebase-logger.js
@@ -18,12 +18,36 @@ const db = firebase.firestore();
 
 const locationReference = db.collection('/company/yDOcLJggM9S9nUNt1SuQ/location');
 
+let socketServer = net.createServer();
+socketServer.listen("./log.sock", ()=>{
+    console.log("Listening");
+});
+socketServer.on("connection", (s)=>{
+    s.on('data',(data)=>{
+        str = data.toString();
+        console.log("data recieved")
+        console.log(str);
+        js = JSON.parse(str);
+        console.log(js);
+        if (!js.reservationId || !js.counter){
+            console.log("fehlerhaftes Objekt");
+        }
+        else{
+            writeBack(js.reservationId,js.counter)
+        }
+    })
+    console.log("Connection");
+});
+socketServer.on("data", (data)=>{
+    console.log(data.toString());
+});
+
 // Funktion zum zuückschreiben des erhöhten Zählers
 function writeBack(reservationId, serviceCounter){
     db.collection('reservation').doc(reservationId).update({counter : serviceCounter})
 }
 
-writeBack('cRHI274cWpEej4dAIJR8',2) // Testen, funktioniert
+//writeBack('cRHI274cWpEej4dAIJR8',2) // Testen, funktioniert
 
 // TODO: Socket implementieren
 // TODO: Bei empfangenen Daten auf Socket writeBack Funktion ausführen
