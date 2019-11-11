2341b61148048b8c427de536a58220a989e9628c
jbol14 <janek.boll@gmx.de>
2019-11-11 20:14:15 +0100

    LocationController kümmert sich um Services Reservierungen Freigaben

---
+++ domain/locationcontroller.py
@@ -0,0 +1,108 @@
+import os
+import datetime
+import json
+from service import Service
+from reservation import Reservation
+from share import Share
+
+class LocationController:
+    ## Globale Variablen
+    RESERVATIONPATH = "./reservation.json"
+    SHAREPATH = "./shares.json"
+    SERVICEPATH = "./services.json"
+    services = {}
+    reservations = {}
+    shares = {}
+    
+    ## Konstruktor
+    def __init__(self):
+        print("Initializing Location")
+
+        # Services initialisieren
+        servicesDict = self.readJsonFile(self.SERVICEPATH)
+        for service in servicesDict:
+            self.services[service] = Service(service, servicesDict[service]["category"],servicesDict[service]["gpioPin"])
+        
+        # Reservierungen initialisieren‚
+        reservationDict = self.readJsonFile(self.RESERVATIONPATH)
+        for reservation in reservationDict:
+            self.reservations[reservation] = Reservation(reservation, reservationDict[reservation], self.services[reservationDict[reservation]["serviceID"]])
+        
+        # Shares initialisieren
+        sharesDict = self.readJsonFile(self.SHAREPATH)
+        for share in sharesDict:
+            self.shares[share] = Share(share, sharesDict[share], self.reservations[sharesDict[share]["reservationID"]])
+
+
+    ''' Liest gegebene JSON-Datei und gibt Inhalt als Dictionary zurück '''
+    def readJsonFile(self, path):
+        try:
+            file = open(path, "rt")
+            fileContent = json.loads(file.read())
+        except FileNotFoundError:
+            file = open(path, "x")
+            fileContent = {}
+        
+        return fileContent
+    
+    ''' Schreibt gegebenes Dictionary data in eine unter path spezifizierte Datei '''
+    def writeJsonFile(self, path, data):
+        try:
+            file = open(path,"w")
+            file.write(data)
+            file.close()
+        except FileNotFoundError:
+            print("Invalid Path")
+    
+    ## Updating
+    ## Resevierung
+    def updateReservation(self, reservation):
+        self.reservations[reservation["id"]] = Reservation(reservation["id"], reservation["payload"], self.services[reservation["payload"]["serviceID"]])
+        # self.writeJsonFile(self.RESERVATIONPATH, json.dumps(self.reservations))
+    
+    ## Share
+    def updateShare(self, share):
+        self.shares[share["id"]] = Share(share["id"], share["payload"], self.reservations[share["payload"]["reservationID"]])
+        #self.writeJsonFile(self.SHAREPATH, self.shares)
+
+    ## Löschen
+    def deleteReservation(self, reservationID):
+        ## Shares löschen, die zu dieser Reservierung gehören
+        idsOfSharesToDelete = []
+        for share in self.shares:
+            if self.shares[share].reservationID == reservationID:
+                idsOfSharesToDelete.append(share)
+        for id in idsOfSharesToDelete:
+            del self.shares[id]
+        try:
+            del self.reservations[reservationID]
+        except KeyError:
+            print("Keine Reservierung mit dieser ID")
+
+    def deleteShare(self, shareID):
+        try:
+            del self.shares[shareID]
+        except KeyError:
+            print("Keine Freigabe mit dieser ID")
+
+    ## Checking
+    def checkReservation(self, key):
+        foundReservation = False
+        for reservation in self.reservations:
+            if self.reservations[reservation].qrCode == key:
+                timeNow = datetime.datetime.now().timestamp() * 1000
+                if timeNow >= self.reservations[reservation].resFrom and timeNow <= self.reservations[reservation].resTill:
+                    foundReservation = True
+                    self.reservations[reservation].useReservation()
+        return foundReservation
+    
+    ##Subject to Change
+    def checkShare(self, key):
+        foundShare = False
+        for share in self.shares:
+            if self.shares[share].qrCode == key:
+                timeNow = datetime.datetime.now().timestamp() * 1000
+                if timeNow >= self.reservations[self.shares[share].reservationID].resFrom and timeNow <= self.reservations[self.shares[share].reservationID].resTill:
+                    self.shares[share].useShare()
+                    foundShare = True
+        return foundShare
