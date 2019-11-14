213b0900a0ecaaa10448512b1d54b23f834650f4
jbol14 <janek.boll@gmx.de>
2019-11-14 16:19:14 +0100

    create JSON from Dictionary check if there are services before instantiating resrvations from backup check if there are reservations before instantiating shares from backup update services

--- domain/locationcontroller.py
+++ domain/locationcontroller.py
@@ -24,14 +24,16 @@ class LocationController:
             self.services[service] = Service(service, servicesDict[service]["category"],servicesDict[service]["gpioPin"])
         
         # Reservierungen initialisieren‚
-        reservationDict = self.readJsonFile(self.RESERVATIONPATH)
-        for reservation in reservationDict:
-            self.reservations[reservation] = Reservation(reservation, reservationDict[reservation], self.services[reservationDict[reservation]["serviceID"]])
+        if not self.services == {}:
+            reservationDict = self.readJsonFile(self.RESERVATIONPATH)
+            for reservation in reservationDict:
+                self.reservations[reservation] = Reservation(reservation, reservationDict[reservation], self.services[reservationDict[reservation]["serviceID"]])
         
         # Shares initialisieren
-        sharesDict = self.readJsonFile(self.SHAREPATH)
-        for share in sharesDict:
-            self.shares[share] = Share(share, sharesDict[share], self.reservations[sharesDict[share]["reservationID"]])
+        if not self.reservations == {}:
+            sharesDict = self.readJsonFile(self.SHAREPATH)
+            for share in sharesDict:
+                self.shares[share] = Share(share, sharesDict[share], self.reservations[sharesDict[share]["reservationID"]])
 
 
     ''' Liest gegebene JSON-Datei und gibt Inhalt als Dictionary zurück '''
@@ -58,12 +60,17 @@ class LocationController:
     ## Resevierung
     def updateReservation(self, reservation):
         self.reservations[reservation["id"]] = Reservation(reservation["id"], reservation["payload"], self.services[reservation["payload"]["serviceID"]])
-        # self.writeJsonFile(self.RESERVATIONPATH, json.dumps(self.reservations))
+        self.writeJsonFile(self.RESERVATIONPATH, json.dumps(self.createJsonFromDict(self.reservations)))
     
     ## Share
     def updateShare(self, share):
         self.shares[share["id"]] = Share(share["id"], share["payload"], self.reservations[share["payload"]["reservationID"]])
-        #self.writeJsonFile(self.SHAREPATH, self.shares)
+        self.writeJsonFile(self.SHAREPATH, json.dumps(self.createJsonFromDict(self.shares)))
+    
+    ## Service
+    def updateService(self, service):
+        self.services[service["id"]] = Service(service["id"], service["payload"]["category"])
+        self.writeJsonFile(self.SERVICEPATH, json.dumps(self.createJsonFromDict(self.services)))
 
     ## Löschen
     def deleteReservation(self, reservationID):
@@ -74,14 +81,17 @@ class LocationController:
                 idsOfSharesToDelete.append(share)
         for id in idsOfSharesToDelete:
             del self.shares[id]
+        self.writeJsonFile(self.SHAREPATH, json.dumps(self.createJsonFromDict(self.shares)))
         try:
             del self.reservations[reservationID]
+            self.writeJsonFile(self.RESERVATIONPATH, json.dumps(self.createJsonFromDict(self.reservations)))
         except KeyError:
             print("Keine Reservierung mit dieser ID")
 
     def deleteShare(self, shareID):
         try:
             del self.shares[shareID]
+            self.writeJsonFile(self.SHAREPATH, json.dumps(self.createJsonFromDict(self.shares)))
         except KeyError:
             print("Keine Freigabe mit dieser ID")
 
@@ -106,3 +116,10 @@ class LocationController:
                     self.shares[share].useShare()
                     foundShare = True
         return foundShare
+
+
+    def createJsonFromDict(self, dictionary):
+        placeholder = {}
+        for element in dictionary:
+            placeholder[element] = dictionary[element].toDict()
+        return placeholder
