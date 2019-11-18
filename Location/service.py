1fd669ee2c38c762b27853192759b42f3e5378c4
jbol14 <janek.boll@gmx.de>
2019-11-18 15:37:19 +0100

    Raspi imorte und GPIO nicht mehr auskommentiert

--- Location/service.py
+++ Location/service.py
@@ -1,12 +1,12 @@
-#import RPi.GPIO as GPIO
-#import time
+import RPi.GPIO as GPIO
+import time
 import json
 
 class Service:
     def __init__(self,id,category, gpioPin=24):
         ## GPIO Setup
-        #GPIO.setmode(GPIO.BCM)
-        #GPIO.setup(gpioPin, GPIO.OUT)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(gpioPin, GPIO.OUT)
 
         self.id = id
         self.category = category
@@ -14,9 +14,9 @@ class Service:
     
     def open(self):
         print("Opened Service {} at Pin {}".format(self.id, self.gpioPin))
-        #GPIO.output(self.gpioPin, GPIO.HIGH)
-        #time.sleep(0.2)
-        #GPIO.output(self.gpioPin, GPIO.LOW)
+        GPIO.output(self.gpioPin, GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(self.gpioPin, GPIO.LOW)
     
     def getId(self):
         return self.id
