eda0fc531c816d596d468e44479edb307fccd40d
jbol14 <janek.boll@gmx.de>
2019-11-28 16:08:02 +0100

    getGPIOPin angepasst

--- Location/service.py
+++ Location/service.py
@@ -1,35 +1,36 @@
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 import time
 import json
 
 class Service:
-    def __init__(self,id,category, gpioPin=24):
+    def __init__(self,id,category, box):
         ## GPIO Setup
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(gpioPin, GPIO.OUT)
+        # GPIO.setmode(GPIO.BCM)
+        # GPIO.setup(gpioPin, GPIO.OUT)
 
         self.id = id
         self.category = category
-        self.gpioPin = gpioPin
+        self.box = box
     
     def open(self):
-        print("Opened Service {} at Pin {}".format(self.id, self.gpioPin))
-        GPIO.output(self.gpioPin, GPIO.HIGH)
-        time.sleep(0.2)
-        GPIO.output(self.gpioPin, GPIO.LOW)
+        print("Opened Service {} at Pin {}".format(self.id, self.getGpioPin()))
+        # GPIO.output(self.gpioPin, GPIO.HIGH)
+        # time.sleep(0.2)
+        # GPIO.output(self.gpioPin, GPIO.LOW)
     
     def getId(self):
         return self.id
 
     def getGpioPin(self):
-        return self.gpioPin
+        return self.box.pin.pinNumber
     
     def toDict(self):
+        print("Box: ", type(self.box))
         return {
             "id" : self.id,
             "category" : self.category,
-            "gpioPin" : self.gpioPin
-            }
+            "box" : self.box.toDict()
+        }
     
     def toJson(self):
         return json.dumps(self.toDict())
